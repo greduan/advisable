4b5c0dd8908e08c8b468e4d083ecdcc5dabc6544
greduan <me@greduan.com>
2016-06-14 08:49:14 -0500

    Fix tests , especially those that test order

--- test/index.js
+++ test/index.js
@@ -14,8 +14,8 @@ describe('advisable', function () {
   it('Should return function with functions .before and .after', function () {
     var res = ad(function () {});
 
-    assert(typeof ad.before, 'function');
-    assert(typeof ad.after, 'function');
+    assert.equal(typeof res.before, 'function');
+    assert.equal(typeof res.after, 'function');
   });
 
   it('Should return function which returns Promise when called', function () {
@@ -23,7 +23,7 @@ describe('advisable', function () {
 
     var prom = res();
 
-    assert(typeof res.then, 'function');
+    assert.equal(typeof prom.then, 'function');
   });
 
   it('Should call functions passed to .before', function () {
@@ -84,12 +84,12 @@ describe('advisable', function () {
 
   it('Should call .before functions in order', function () {
     var res = ad(function () {});
-    var d = td.function();
+    var order = [];
 
     res.before(function () {
       return new Promise(function (resolve) {
         setTimeout(function () {
-          d('one');
+          order.push(1);
 
           resolve();
         }, 100);
@@ -97,27 +97,25 @@ describe('advisable', function () {
     });
 
     res.before(function () {
-      d('two');
+      order.push(2);
 
       return Promise.resolve();
     });
 
     return res()
       .then(function () {
-        // TODO: Doesn't properly test for order
-        td.verify(d('one'));
-        td.verify(d('two'));
+        assert.deepEqual(order, [1, 2]);
       });
   });
 
   it('Should call .after functions in order', function () {
     var res = ad(function () {});
-    var d = td.function();
+    var order = [];
 
     res.after(function () {
       return new Promise(function (resolve) {
         setTimeout(function () {
-          d('one');
+          order.push(1);
 
           resolve();
         }, 100);
@@ -125,27 +123,25 @@ describe('advisable', function () {
     });
 
     res.after(function () {
-      d('two');
+      order.push(2);
 
       return Promise.resolve();
     });
 
     return res()
       .then(function () {
-        // TODO: Doesn't properly test for order
-        td.verify(d('one'));
-        td.verify(d('two'));
+        assert.deepEqual(order, [1, 2]);
       });
   });
 
   it('Should call .after functions AFTER .before functions', function () {
     var res = ad(function () {});
-    var d = td.function();
+    var order = [];
 
     res.before(function () {
       return new Promise(function (resolve) {
         setTimeout(function () {
-          d('one');
+          order.push(1);
 
           resolve();
         }, 100);
@@ -153,16 +149,14 @@ describe('advisable', function () {
     });
 
     res.after(function () {
-      d('two');
+      order.push(2);
 
       return Promise.resolve();
     });
 
     return res()
       .then(function () {
-        // TODO: Doesn't properly test for order
-        td.verify(d('one'));
-        td.verify(d('two'));
+        assert.deepEqual(order, [1, 2]);
       });
   });
 
